890ba6629bc2c06ba8043c1586e79ce4f6674c4c
M Ehtesham Ul Hassan Malik <153924478+M-Ehtesham-Ul-Hassan-Malik@users.noreply.github.com>
2024-02-04 11:47:27 +0500

    List and its methods

---
+++ 04_class(list & its methods)/Class04.py
@@ -0,0 +1,6 @@
+
+# ->                    0        1         2
+names : list[str] = ["Amjad","Fatima Zahra", "M Ehtesham ul Hassan Malik"]
+# <-                    -3     -2          -1
+
+print(names[-2])
