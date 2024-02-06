40295fa55adefebe2fd00b55279828fba87da8e7
M Ehtesham Ul Hassan Malik <153924478+M-Ehtesham-Ul-Hassan-Malik@users.noreply.github.com>
2024-02-06 13:37:34 +0500

    List, Tuple & for loop

---
+++ 05_class(list_tuple_for_loop)/Class05.py
@@ -0,0 +1,4 @@
+data_base : list[tuple[str,str]] = [("Amjad",'123'),
+                                    ("Ali",'345'),
+                                    ('Ahmed','789')
+                                    ]
