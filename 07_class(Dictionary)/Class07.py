872f2de45a7f0d47340cd1ecbf60c22fe9992d36
M Ehtesham Ul Hassan Malik <153924478+M-Ehtesham-Ul-Hassan-Malik@users.noreply.github.com>
2024-02-18 08:20:35 +0500

    Dictionary, Dict comprehensive

---
+++ 07_class(Dictionary)/Class07.py
@@ -0,0 +1,11 @@
+from typing import Dict,Union
+
+Key = Union[int, str]
+Key = Union[int, str, set, dict, list, tuple]
+
+data : Dict[str, str] = {"Name":"Ahmed",
+                        "F'Name":"Abdul Razzaq",
+                        "Education":"Matric"
+                        }
+
+print(data)
